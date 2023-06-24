aa95f335b6654cd97cde19f5d04caf0ba566e804
Angel Figuera <angeleraser@gmail.com>
2023-06-24 11:47:08 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./assets/css/index.css";
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum atque
+      voluptates assumenda optio. Assumenda distinctio sequi nam sed. Qui
+      blanditiis velit quasi? Nemo blanditiis alias cupiditate adipisci quia
+      dolores hic.
+    </div>
+  );
+}
+
+export default App;
