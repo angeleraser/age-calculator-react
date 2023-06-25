1977cd0c8b90a541b667654568d56fbcd31f7a0e
Angel Figuera <angeleraser@gmail.com>
2023-06-24 22:24:54 -0400

    feat: add Heading component and improve form layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import "./assets/css/index.css";
 import { Card } from "./components/Card/Card";
 
 import { AgeForm } from "./components/AgeForm/AgeForm";
+import { Heading } from "./components/Heading/Heading";
 
 function App() {
   return (
     <Card maxWidth="840px">
       <AgeForm onSubmit={console.log} />
+
+      <div className="headings-container">
+        <Heading value="" label="years" />
+        <Heading value="" label="months" />
+        <Heading value="" label="days" />
+      </div>
     </Card>
   );
 }
