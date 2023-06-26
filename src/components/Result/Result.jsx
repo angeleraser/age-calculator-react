be12eef36b5a4404c6b1e96e8f1db84fd24324c7
Angel Figuera <angeleraser@gmail.com>
2023-06-25 21:05:51 -0400

    fix: improve app main styles

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -3,7 +3,7 @@ import "./Result.css";
 export const Result = ({ value = "", label = "", subLabel = "" }) => {
   return (
     <h2 className="result">
-      <span className="result-value">{value || "--"}</span>
+      <span className="result-value notranslate">{value || "--"}</span>
       <span className="result-label"> {label}</span>
       <span className="result-sublabel">{subLabel}</span>
     </h2>
